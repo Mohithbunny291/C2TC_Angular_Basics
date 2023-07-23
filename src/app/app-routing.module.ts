f6520ba7c85339769dc2e8637a04c3b314b99a25
Mohith Reddy <50856290+Mohithbunny291@users.noreply.github.com>
2023-07-23 12:02:15 +0530

    Final Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentListRComponent } from './department-list-r/department-list-r.component';
+import { EmployeeListRComponent } from './employee-list-r/employee-list-r.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'departments', component: DepartmentListRComponent},
+  {path: 'employees', component: EmployeeListRComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents: any = { DepartmentListRComponent, EmployeeListRComponent }
